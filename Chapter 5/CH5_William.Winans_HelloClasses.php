34220532d8f3b71c295524b0bff69b532f9fbea5
William Winans <william.winans@tstc.edu>
2016-11-09 13:03:08 -0600

    Minor Tweak
    Missed some code in last update

--- Chapter 5/CH5_William.Winans_HelloClasses.php
+++ Chapter 5/CH5_William.Winans_HelloClasses.php
@@ -7,6 +7,10 @@ Chapter 5 PHP code assignment
 */
 include_once('CH5_William.Winans_HelloFunctions.php');
 
+$MyName = 'wIlLiAm wiNANs';
+
+echo fix_name($MyName) . "<br>" . PHP_EOL;
+
 $objUser = new PotentialUser("wrwinans", "William", "Winans", "SomeP@SSw0rd", "903-555-1212", "william.winans@tstc.edu", "2650 S East End Blvd, Marshall, Tx 75670");
 $objEnrolledUser = new EnrolledUser("ITSE 1306", "PHP Programming", "Will Winans", $objUser->username, $objUser->fname, $objUser->lname, $objUser->password, $objUser->pnumber, $objUser->email, $objUser->phy_address);
 
--- Chapter 5/CH5_William.Winans_Hellofunctions.php
+++ Chapter 5/CH5_William.Winans_Hellofunctions.php
@@ -5,10 +5,6 @@ ITSE 1306
 2017.01.17
 Chapter 5 PHP code assignment
 */
-$MyName = 'wIlLiAm wiNANs';
-
-echo fix_name($MyName) . "<br>" . PHP_EOL;
-
 function fix_name($n)
 {
 	$n = ucwords(strtolower($n));
