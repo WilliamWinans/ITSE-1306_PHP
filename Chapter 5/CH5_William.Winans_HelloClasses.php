3922faa2c377b91542cc3adf4454d2efd5ce5666
William Winans <william.winans@tstc.edu>
2016-11-09 13:00:37 -0600

    Homework Adjustments
    Made adjustments to the homework assignment. Needed to update the files
    to match.

--- Chapter 5/CH5_William.Winans_HelloClasses.php
+++ Chapter 5/CH5_William.Winans_HelloClasses.php
@@ -7,48 +7,6 @@ Chapter 5 PHP code assignment
 */
 include_once('CH5_William.Winans_HelloFunctions.php');
 
-Class PotentialUser
-{
-	public $username = "username";
-	public $fname = "fname";
-	public $lname = "lname";
-	public $password = "password";
-	public $pnumber = "pnumber";
-	public $email = "email";
-	public $phy_address = "phy_address";
-	
-	function __construct($username, $fname, $lname, $password, $pnumber, $email, $phy_address)
-	{
-		$this -> username = $username;
-		$this -> fname = $fname;
-		$this -> lname = $lname;
-		$this -> password = $password;
-		$this -> pnumber = $pnumber;
-		$this -> email = $email;
-		$this -> phy_address = $phy_address;
-	}
-	
-};
-
-class EnrolledUser extends PotentialUser
-{
-	public $class_name, $class_subject, $instructor_name;
-	
-	function __construct($class_name, $class_subject, $instructor_name, $username, $fname, $lname, $password, $pnumber, $email, $phy_address)
-	{
-		$this -> class_name = $class_name;
-		$this -> class_subject = $class_subject;
-		$this -> instructor_name = $instructor_name;
-		$this -> username = $username;
-		$this -> fname = $fname;
-		$this -> lname = $lname;
-		$this -> password = $password;
-		$this -> pnumber = $pnumber;
-		$this -> email = $email;
-		$this -> phy_address = $phy_address;
-	}
-};
-
 $objUser = new PotentialUser("wrwinans", "William", "Winans", "SomeP@SSw0rd", "903-555-1212", "william.winans@tstc.edu", "2650 S East End Blvd, Marshall, Tx 75670");
 $objEnrolledUser = new EnrolledUser("ITSE 1306", "PHP Programming", "Will Winans", $objUser->username, $objUser->fname, $objUser->lname, $objUser->password, $objUser->pnumber, $objUser->email, $objUser->phy_address);
 
--- Chapter 5/CH5_William.Winans_Hellofunctions.php
+++ Chapter 5/CH5_William.Winans_Hellofunctions.php
@@ -15,6 +15,46 @@ function fix_name($n)
 	return $n;
 }
 
+Class PotentialUser
+{
+	public $username = "username";
+	public $fname = "fname";
+	public $lname = "lname";
+	public $password = "password";
+	public $pnumber = "pnumber";
+	public $email = "email";
+	public $phy_address = "phy_address";
+	
+	function __construct($username, $fname, $lname, $password, $pnumber, $email, $phy_address)
+	{
+		$this -> username = $username;
+		$this -> fname = $fname;
+		$this -> lname = $lname;
+		$this -> password = $password;
+		$this -> pnumber = $pnumber;
+		$this -> email = $email;
+		$this -> phy_address = $phy_address;
+	}
+	
+};
 
+class EnrolledUser extends PotentialUser
+{
+	public $class_name, $class_subject, $instructor_name;
+	
+	function __construct($class_name, $class_subject, $instructor_name, $username, $fname, $lname, $password, $pnumber, $email, $phy_address)
+	{
+		$this -> class_name = $class_name;
+		$this -> class_subject = $class_subject;
+		$this -> instructor_name = $instructor_name;
+		$this -> username = $username;
+		$this -> fname = $fname;
+		$this -> lname = $lname;
+		$this -> password = $password;
+		$this -> pnumber = $pnumber;
+		$this -> email = $email;
+		$this -> phy_address = $phy_address;
+	}
+};
 
 ?>
